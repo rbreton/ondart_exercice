e28e24702ceb23f6c265b64a746c88d501aa4213
rbreton <bretonr@gmail.com>
2014-01-20 12:03:31 -0500

    MAJ ex01 espacement

--- ex01/ex01.dart
+++ ex01/ex01.dart
@@ -1,9 +1,17 @@
 void main() {
   int intBase = 1;
   int intLenght = 9;
+  int intLenghtCpt = intLenght*2;
   for (int n = 1; n<=intLenght; n++){
     int intMult = intBase * intBase;
-    print('${intBase} * ${intBase} = ${intMult}');
+    
+    String strSpacer = '';
+    for (int m = 1; m<= intLenghtCpt; m++){
+      strSpacer = strSpacer + ' ';
+    }
+    intLenghtCpt = intLenghtCpt - 2;
+    
+    print('${strSpacer}${intBase} * ${intBase} = ${intMult}');
     String strBase = intBase.toString() + '1';
     intBase = int.parse(strBase);
   }
