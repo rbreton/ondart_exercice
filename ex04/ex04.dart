d6bf6ea653d2d3271cf70550127606ffceda9376
rbreton <bretonr@gmail.com>
2014-02-06 16:36:03 -0500

    UPDATE ex04

--- ex04/ex04.dart
+++ ex04/ex04.dart
@@ -1,8 +1,12 @@
 void main(){
   print(fibonacci(11));
+  print(endPointLine(980, 660));
+  print(passedTimeBetween(new DateTime(1991, 09, 22, 24, 0)));
+  print(investment(10, 1000, 3.3));
+  print(timesTable(12, 20));
 }
 String fibonacci(int intNI){
-  String strPrint = '0, ';
+  String strPrint = '#1 La suite de fibonacci :\n 0, ';
   int intCpt = 1;
   int intA = 0;
   int intB = 1;
@@ -14,6 +18,32 @@ String fibonacci(int intNI){
     strPrint = strPrint + intCalResult.toString() + strSpacer;
     intB = intA;
     intA = intCalResult;
+    intCpt++;
+  }
+  return strPrint;
+}
+
+String endPointLine(int intW, int intH){
+  String strPrint = '#2  :\n';
+  return strPrint;
+}
+
+String passedTimeBetween(DateTime DTBirthday){
+  String strPrint = '#3 Temps :\n';
+  return strPrint;
+}
+
+String investment(int intYears, int intInvestment, double dblInterest){
+  String strPrint = '#4 :\n';
+  return strPrint;
+}
+
+String timesTable(int intTimeTable, int intNI){
+  String strPrint = '#5 Les $intNI premiers chiffres de la table de multiplication $intTimeTable :\n';
+  int intCpt = 1;
+  while (intCpt <= intNI){
+    int intMult = intCpt * intTimeTable;
+    strPrint += ' $intCpt X $intTimeTable = $intMult\n';
     intCpt++;
   }
   return strPrint;
