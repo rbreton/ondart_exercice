e71240e7a8aa69447dbe66e57c095a2c71445b65
rbreton <bretonr@gmail.com>
2014-02-13 18:21:09 -0500

    UPADTE ex05

--- ex05/bin/ex05.dart
+++ ex05/bin/ex05.dart
@@ -1,5 +1,9 @@
 import 'package:ex05/fcns.dart';
 
 void main(){
-  onlyLetters();
+  print(onlyLetters('salut coMmmnet'));
+  print(longestSentenceIn('lblblblblbblb bnjgjfd. asjldhsjkldhasljdha. sdjklfhlkasdhf.'));
+  print(longestWordIn('Blah blaj sdhaasdsads'));
+  print(timesTable(8));
+  print(displayTree(8));
 }
--- ex05/lib/fcns.dart
+++ ex05/lib/fcns.dart
@@ -1,5 +1,47 @@
 library fcns;
 
 String onlyLetters(String strText){
+  String strReturn;
+  RegExp exp = new RegExp(r"^[a-zA-Z ]*$");
+  strReturn = exp.hasMatch(strText).toString();
+  return strReturn;
+}
+String longestSentenceIn(String strText){
+  String strReturn = '';
+  List lstSentence = strText.split('.');
   
+  return strReturn;
+}
+String longestWordIn(String strText){
+  String strReturn = '';
+  List lstSentence = strText.split(' ');
+  
+  return strReturn;
+}
+String displayTree(int intHeight){
+  String strReturn = '';
+  int intCptSpacer = intHeight;
+  for (int n = 1; n <= intHeight; n++){
+    String strSpacer = '';
+    for (int m = 1; m<= intCptSpacer; m++){
+      strSpacer = strSpacer + ' ';
+    }
+    intCptSpacer--;
+    strReturn += strSpacer;
+    for (int o = 1; o <= n; o++){
+      strReturn += '*';
+    }
+    strReturn += '\n';
+  }
+  return strReturn;
+}
+String timesTable(int intTimesTable){
+  String strReturn = '';
+  int intCpt = 1;
+  while (intCpt <= 12){
+    int intMult = intCpt * intTimesTable;
+    strReturn += ' $intCpt X $intTimesTable = $intMult\n';
+    intCpt++;
+  }
+  return strReturn + '\n';
 }
