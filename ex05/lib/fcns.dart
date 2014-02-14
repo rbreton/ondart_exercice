4dcb8fecd5d93e6fc8b17ab3bd66560a8af7f4a9
rbreton <bretonr@gmail.com>
2014-02-14 10:02:28 -0500

    UPDATE ex05

--- ex05/lib/fcns.dart
+++ ex05/lib/fcns.dart
@@ -9,14 +9,36 @@ String onlyLetters(String strText){
 String longestSentenceIn(String strText){
   String strReturn = '';
   List lstSentence = strText.split('.');
-  
+  int intLongestPosition = 0;
+  for(int n = 0; n <= lstSentence.length - 1; n++){
+    if(lstSentence[n].length > lstSentence[intLongestPosition].length){
+      intLongestPosition = n;
+    }
+  }
+  strReturn = lstSentence[intLongestPosition].toString() + '.';
   return strReturn;
 }
 String longestWordIn(String strText){
   String strReturn = '';
-  List lstSentence = strText.split(' ');
-  
+  List lstWord = strText.split(' ');
+  int intLongestPosition = 0;
+  for(int n = 0; n <= lstWord.length - 1; n++){
+    if(lstWord[n].length > lstWord[intLongestPosition].length){
+      intLongestPosition = n;
+    }
+  }
+  strReturn = lstWord[intLongestPosition].toString();
   return strReturn;
+}
+String timesTable(int intTimesTable){
+  String strReturn = '';
+  int intCpt = 1;
+  while (intCpt <= 12){
+    int intMult = intCpt * intTimesTable;
+    strReturn += ' $intCpt X $intTimesTable = $intMult\n';
+    intCpt++;
+  }
+  return strReturn + '\n';
 }
 String displayTree(int intHeight){
   String strReturn = '';
@@ -34,14 +56,4 @@ String displayTree(int intHeight){
     strReturn += '\n';
   }
   return strReturn;
-}
-String timesTable(int intTimesTable){
-  String strReturn = '';
-  int intCpt = 1;
-  while (intCpt <= 12){
-    int intMult = intCpt * intTimesTable;
-    strReturn += ' $intCpt X $intTimesTable = $intMult\n';
-    intCpt++;
-  }
-  return strReturn + '\n';
 }
