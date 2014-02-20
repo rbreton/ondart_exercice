535b720ff1bd8a1af07833475d4d354076b9d9db
rbreton <bretonr@gmail.com>
2014-02-20 15:22:07 -0500

    ADD ex06 - start

---
+++ ex06/bin/ex06.dart
@@ -0,0 +1,6 @@
+import 'package:ex06/fcns.dart';
+
+void main(){
+  print(palindrome('salut coMmmnet'));
+  print(numDaysBetween('sdsds'));
+}
---
+++ ex06/lib/fcns.dart
@@ -0,0 +1,12 @@
+library fcns;
+
+String palindrome(String strText){
+  String strReturn;
+
+  return strReturn + '\n';
+}
+String numDaysBetween(String strText){
+  String strReturn;
+
+  return strReturn + '\n';
+}
