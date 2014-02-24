37233e7d5c1e168e865350d736fe6459beb97acf
rbreton <bretonr@gmail.com>
2014-02-24 12:51:31 -0500

    UPDATE ex06

--- ex06/bin/ex06.dart
+++ ex06/bin/ex06.dart
@@ -2,5 +2,8 @@ import 'package:ex06/fcns.dart';
 
 void main(){
   print(palindrome('salut coMmmnet'));
-  print(numDaysBetween('sdsds'));
+  print(numDaysBetween(new DateTime(1991, 09, 22, 0, 0), new DateTime(1991, 10, 22, 0, 0)));
+  print(convertNumGradesToLetters());
+  print(getNamelength());
+  print(getClubPlayers());
 }
--- ex06/lib/fcns.dart
+++ ex06/lib/fcns.dart
@@ -5,7 +5,25 @@ String palindrome(String strText){
 
   return strReturn + '\n';
 }
-String numDaysBetween(String strText){
+String numDaysBetween(DateTime dtFirstDate, DateTime dtSecondDate){
+  String strReturn;
+  Duration drtPassedTime = dtSecondDate.difference(dtFirstDate);
+  
+  int intDaysPassed = drtPassedTime.inDays;
+  strReturn += 'Il y a $intDaysPassed';
+  return strReturn + '\n';
+}
+String convertNumGradesToLetters(){
+  String strReturn;
+
+  return strReturn + '\n';
+}
+String getNamelength(){
+  String strReturn;
+
+  return strReturn + '\n';
+}
+String getClubPlayers(){
   String strReturn;
 
   return strReturn + '\n';
