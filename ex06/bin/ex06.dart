5ed8ec00fb1830ab5c8a1d76af79bf5395b7535b
rbreton <bretonr@gmail.com>
2014-02-27 13:02:59 -0500

    UPADTE ex06

--- ex06/bin/ex06.dart
+++ ex06/bin/ex06.dart
@@ -3,7 +3,7 @@ import 'package:ex06/fcns.dart';
 void main(){
   print(palindrome("À l'étape, épate-la !"));
   print(numDaysBetween(new DateTime(1991, 09, 22, 0, 0), new DateTime(1991, 09, 23, 0, 0)));
-  print(convertNumGradesToLetters(72));
+  print(convertNumGradesToLetters(72.00));
   print(getNamelength(['Rémi','Jake','Jo']));
   print(getClubPlayers());
 }
--- ex06/lib/fcns.dart
+++ ex06/lib/fcns.dart
@@ -49,9 +49,30 @@ String numDaysBetween(DateTime dtFirstDate, DateTime dtSecondDate){
   strReturn = 'Il y a $intDaysPassed jour${getPlural(intDaysPassed, 's')} entre le ${dtFirstDate.day} ${lstMois[dtFirstDate.month-1]} ${dtFirstDate.year} et le ${dtSecondDate.day} ${lstMois[dtSecondDate.month-1]} ${dtSecondDate.year}.';
   return strReturn + '\n';
 }
-String convertNumGradesToLetters(int intNumGrade){
-  String strReturn = '';
-  
+String convertNumGradesToLetters(double dblNumGrade){
+  String strReturn = 'Votre note de $dblNumGrade % vous donne un ';
+  Map mapGrades = new Map();
+  mapGrades = {
+    'A+' : {'max' : 100, 'min' : 95},
+    'A' : {'max' :  94.99, 'min' : 91.5},
+    'A-' : {'max' : 91.49, 'min' : 88},
+    'B+' : {'max' : 87.99, 'min' : 84.5},
+    'B' : {'max' : 84.49, 'min' : 81},
+    'B-' : {'max' : 80.99, 'min' : 77.5},
+    'C+' : {'max' : 77.49, 'min' : 74},
+    'C' : {'max' : 73.99, 'min' : 70.5},
+    'C-' : {'max' : 70.49, 'min' : 67},
+    'D+' : {'max' : 66.99, 'min' : 63.5},
+    'D' : {'max' : 63.49, 'min' : 60},
+    'E' : {'max' : 59.99, 'min' : 0}
+  };
+  void getConvertNumGradesToLetters(key, value){
+    if(dblNumGrade <= value['max'] && dblNumGrade >= value['min']){
+      strReturn += key;
+    }
+  }
+  mapGrades.forEach(getConvertNumGradesToLetters);
+  strReturn += '.';
   return strReturn + '\n';
 }
 String getNamelength(List lstNames){
