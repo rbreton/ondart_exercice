8fae3f05af496cd4c3242c380ec5b8324e2d6b40
rbreton <bretonr@gmail.com>
2014-02-28 13:00:23 -0500

    ADD ex 08 start

---
+++ ex08/bin/ex08.dart
@@ -0,0 +1,5 @@
+import 'package:ex08/fcns.dart';
+
+void main(){
+  print(getSentence());
+}
---
+++ ex08/lib/fcns.dart
@@ -0,0 +1,9 @@
+library fcns;
+String getSentence(){
+  String strReturn = '';
+  List lstNouns = ['Berry','Chef','Kangaroo','Partner','Wool','Silver','Pig','Soldier','Magician','Garage'];
+  List lstVerbs = ['blind','develop','ask','force','peel','analyse','applaud','report','paint','join'];
+  List lstAdjectives = ['Noisy','Careful','Secret','Afraid','Embarrassed','Intelligent','Ignorant','Tested','Spotted','Simple'];
+  List lstAdverbs = ['hilarious','fantastic','abusive','cool','special','imaginary','functional','valuable','plant','talented'];
+  return strReturn + '\n';
+}
